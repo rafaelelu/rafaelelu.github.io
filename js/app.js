ceceafef77ed0e3214604f2fe6ca766e478d6525
rafaelelu <a01634309@itesm.mx>
2017-10-16 22:18:06 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation()
+
+var modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function(btn){
+  btn.onmouseover = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
+
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
+    modal.style.display = "none";
+  }
+});
+
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+}
