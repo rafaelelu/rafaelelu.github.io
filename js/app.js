3f4005dcf445565773f9bc4002f5e1281601b606
rafaelelu <a01634309@itesm.mx>
2017-10-17 20:06:09 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).foundation()
 
 var modalBtns = [...document.querySelectorAll(".button")];
 modalBtns.forEach(function(btn){
-  btn.onmouseover = function() {
+  btn.onclick = function() {
     var modal = btn.getAttribute('data-modal');
     document.getElementById(modal).style.display = "block";
   }
